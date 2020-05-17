7bb2ea9f817a3bc760edbcff16c74e209c7467d5
Colton Allen <cmanallen90@gmail.com>
2020-05-17 14:28:45 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var clients = make(map[*websocket.Conn]bool)
+var channel = make(chan Message)
+var socket = websocket.Upgrader{}
+
+type Message struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("../public"))
+	http.Handle("/", fs)
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	log.Println("Server started on port 5000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func handleConnections(response http.ResponseWriter, request *http.Request) {
+	ws, err := socket.Upgrade(response, request, nil)
+	if err != nil {
+		log.Fatal("Request failed... ", err)
+	}
+	defer ws.Close()
+
+	clients[ws] = true
+
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			delete(clients, ws)
+			break
+		}
+		channel <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-channel
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
